ef3b582425486b056c2323009da499805a667539
Shuangrui CHEN <chenshuangrui@gmail.com>
2026-01-16 15:54:50 +0800

    Initial Commit

---
+++ TrendLensTests/TrendLensTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrendLensTests.swift
+//  TrendLensTests
+//
+//  Created by Shuangrui CHEN on 1/16/26.
+//
+
+import Testing
+
+struct TrendLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
